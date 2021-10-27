9ad8b633cf4c9fc06194f3d57976ed28233137a6
Jeremy Thompson <jeremy.thompson@hotmail.ca>
2021-10-27 15:50:51 -0400

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./styles/reset.css";
 
+import ReactGA from "ga-4-react";
+
+const TRACKING_ID = "G-XCBD2GW752";
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
+
+try {
+  setTimeout((_) => {
+    const ga4react = new ReactGA(TRACKING_ID);
+    ga4react.initialize();
+  }, 4000);
+} catch (err) {}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
